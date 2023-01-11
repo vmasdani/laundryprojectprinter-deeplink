86aef691925586a245785d51d67d9118891153c9
vmasdani <valianmasdani@gmail.com>
2023-01-11 23:07:57 +0700

    print

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:convert';
 
+import 'package:blue_thermal_printer/blue_thermal_printer.dart';
 import 'package:flutter/material.dart';
 import 'package:uni_links/uni_links.dart';
+import 'dart:typed_data';
+import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -10,39 +14,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Laundry Printer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Laundry Printer'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -51,31 +36,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Uri? _initialUri;
+  final bluetooth = BlueThermalPrinter.instance;
+  var _state = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var _devices = <BluetoothDevice>[];
+  Uri? _initialUri;
 
   @override
   void initState() {
-    _initialUriGet();
+    _handleInit();
+
+    bluetooth.onStateChanged().listen((state) {
+      switch (state) {
+        case BlueThermalPrinter.CONNECTED:
+          setState(() {
+            _state = 'Connected';
+          });
+          break;
+        case BlueThermalPrinter.DISCONNECTED:
+          setState(() {
+            _state = 'Disconnected';
+          });
+          break;
+        case BlueThermalPrinter.DISCONNECT_REQUESTED:
+          setState(() {
+            _state = 'Disconnect requested';
+          });
+          break;
+        case BlueThermalPrinter.STATE_TURNING_OFF:
+          setState(() {
+            _state = 'Turning off';
+          });
+          break;
+        case BlueThermalPrinter.STATE_OFF:
+          setState(() {
+            _state = 'Off';
+          });
+          break;
+        case BlueThermalPrinter.STATE_ON:
+          setState(() {
+            _state = 'On';
+          });
+          break;
+        case BlueThermalPrinter.STATE_TURNING_ON:
+          setState(() {
+            _state = 'Turning On';
+          });
+          break;
+        case BlueThermalPrinter.ERROR:
+          setState(() {
+            _state = 'Error';
+          });
+          break;
+        default:
+          print(state);
+          break;
+      }
+    });
+
     super.initState();
   }
 
-  Future _initialUriGet() async {
+  Future _handleInit() async {
     final uri = await getInitialUri();
+    try {
+      _devices = await bluetooth.getBondedDevices();
+    } catch (e) {}
+
     setState(() {
       _initialUri = uri;
     });
+  }
+
+  Future _handlePrint(BluetoothDevice d) async {
+    try {
+      await bluetooth.connect(d);
+
+      bluetooth.printCustom('Hello', 2, 0);
+      bluetooth.printQRcode(
+            "Insert Your Own Text to Generate", 200, 200, 1);
+      bluetooth.printNewLine();
+      bluetooth.printNewLine();
+      bluetooth.printNewLine();
+
+      await bluetooth.disconnect();
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
@@ -88,24 +135,56 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(_initialUri.toString()),
-            Text(_initialUri?.queryParameters["uuid"] ?? 'No uuid'),
+            // Text(_initialUri.toString()),
+            Text(_initialUri?.queryParameters["uuid"] ??
+                'No uuid detected. Cannot print.'),
+            Text('Bluetooth : $_state'),
             
+            Column(
+              children: _devices
+                  .map((d) => Container(
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    child: Column(children: [
+                                      Container(
+                                        child: Text(d?.name ?? ''),
+                                      ),
+                                      Container(
+                                        child: Text(d.address ?? ''),
+                                      )
+                                    ]),
+                                  ),
+                                ),
+                                Container(
+                                  child: MaterialButton(
+                                    onPressed: () async {
+                                      _handlePrint(d);
+                                    },
+                                    color: Theme.of(context).primaryColor,
+                                    child: Text(
+                                      'Print',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Divider()
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
